00ea48baa82a7acee4c9bab9674b70f373e9a08b
Catherine Gunita <Catherine_gunita@dlsu.edu.ph>
2025-11-13 15:09:24 +0800

    c

--- src/main/java/com/grp5/controller/MainController.java
+++ src/main/java/com/grp5/controller/MainController.java
@@ -1,5 +1,13 @@
 package com.grp5.controller;
-
+import com.grp5.model.*;
+import com.grp5.dao.*;
 public class MainController {
 
+    public static void main(String[] args){
+        System.out.println("Hello WOrld");
+        customerRecordDAO user=new customerRecordDAO();
+        user.addCustomerRecordData(new customerRecordModel(1, "Gabe", "Bactong", "Bactong@gmail.com", "12345678", "GabbyCheeks"));
+        user.addCustomerRecordData(new customerRecordModel(2, "Cath", "Gunita","Gunita@gmail.com", "12345342", "CathyCheeks"));
+        
+    }
 }
