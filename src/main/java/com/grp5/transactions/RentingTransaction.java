e231153b4a41e01983d8b31db01941cd4a4185ad
Gabrielle Joei Bactong <gabriellejoeibactong@Gabrielles-MacBook-Pro.local>
2025-11-16 10:37:44 +0800

    Feat: implement rentalSalesReport.java

---
+++ src/main/java/com/grp5/transactions/RentingTransaction.java
@@ -0,0 +1,23 @@
+package com.grp5.transactions;
+
+import com.grp5.dao.customerRecordDAO;
+import com.grp5.dao.transactionRecordDAO;
+
+public class RentingTransaction {
+
+    private customerRecordDAO customerDAO;
+    private transactionRecordDAO transactionDAO;
+    
+    // Reading the customer record to retrieve the customer’s account ID
+
+    // Recording the rental reservation reference number
+
+    // Recording the payment reference number upon payment of the rental 
+
+    // Reading the bike record to retrieve the branch ID number
+
+    // Recording of the payment date upon payment of the rental
+
+    // Recording of the payment amount upon payment of the renal
+
+}
