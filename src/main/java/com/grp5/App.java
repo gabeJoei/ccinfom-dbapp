d70d4ff56e1f76148ded4b9d76813e86613f4053
enzorosas <enzorosas90@gmail.com>
2025-11-16 21:01:22 +0800

    fixed filepath

--- src/main/java/com/grp5/App.java
+++ src/main/java/com/grp5/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("AdminOrUser.fxml")); // Get the resources :D
+            Parent root = FXMLLoader.load(getClass().getResource("/com/grp5/view/AdminOrUser.fxml")); // Get the resources :D
             Scene scene = new Scene(root); // Set - up new scene :D
 
             stage.setScene(scene);
