704890f23f6e5196528960c8af29096c907efbeb
Catherine Gunita <Catherine_gunita@dlsu.edu.ph>
2025-11-16 21:09:22 +0800

    c

--- src/main/java/com/grp5/App.java
+++ src/main/java/com/grp5/App.java
@@ -1,5 +1,7 @@
 package com.grp5;
 
+import java.io.IOException;
+
 import com.grp5.utils.databaseConnection;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,12 +14,11 @@ import javafx.stage.Stage;
  * Loads the rentBike.fxml to test UI and controller functionality
  */
 public class App extends Application {
-    
+
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
         try {
-<<<<<<< HEAD
-         Parent root = FXMLLoader.load(getClass().getResource("/com/grp5/view/AdminOrUser.fxml")); // Get the resources :D
+            Parent root = FXMLLoader.load(getClass().getResource("/com/grp5/view/AdminOrUser.fxml")); // Get the resources :D
             Scene scene = new Scene(root); // Set - up new scene :D
 
             stage.setScene(scene);
@@ -27,30 +28,18 @@ public class App extends Application {
         } catch (IOException e) {
             e.getStackTrace();
             System.err.println("Tite");
-=======
-
->>>>>>> d70d4ff56e1f76148ded4b9d76813e86613f4053
         }
     }
-    
+
     @Override
     public void stop() {
         // Clean up when application closes
         databaseConnection.closeConnection();
         System.out.println("\n Application closed. Database connection closed.");
     }
-    
+
     public static void main(String[] args) {
-        printHeader();
         launch(args);
     }
-    
-    /**
-     * Print application header
-     */
-    private static void printHeader() {
-        System.out.println("         RENT BIKE UI TEST            ");
-        System.out.println("   Testing: rentBike.fxml             ");
 
-    }
 }
