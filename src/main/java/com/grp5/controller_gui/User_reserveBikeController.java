c48dbbebef3ad6c8cdfba102225484ede1633c11
Gabrielle Joei Bactong <gabriellejoeibactong@Gabrielles-MacBook-Pro.local>
2025-11-18 01:18:15 +0800

    Feat: implement time

--- src/main/java/com/grp5/controller_gui/User_reserveBikeController.java
+++ src/main/java/com/grp5/controller_gui/User_reserveBikeController.java
@@ -25,6 +25,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -39,6 +41,15 @@ public class User_reserveBikeController {
     private DatePicker startDateSelection;
     @FXML
     private DatePicker endDateSelection;
+
+    @FXML
+    private Spinner<Integer> startHour;
+    @FXML
+    private Spinner<Integer> startMinute;
+    @FXML
+    private Spinner<Integer> endHour;
+    @FXML
+    private Spinner<Integer> endMinute;
 
     @FXML
     private Button reserveButton;
@@ -62,6 +73,28 @@ public class User_reserveBikeController {
         this.bikeModelText.setText(selectedBike.getBikeModel());
         this.bikeCostText.setText(String.format("₱%.2f / day", selectedBike.getDailyRate()));
 
+        // Initialize Spinners
+        SpinnerValueFactory<Integer> startHourSpinner = new SpinnerValueFactory.IntegerSpinnerValueFactory(
+            0, 23, 12 // (min, max, default)
+        ); 
+        startHour.setValueFactory(startHourSpinner);
+
+        SpinnerValueFactory<Integer> endHourSpinner = new SpinnerValueFactory.IntegerSpinnerValueFactory(
+            0, 23, 12
+        );
+        endHour.setValueFactory(endHourSpinner);
+
+        // Configure minute spinners (0-59, in 15-min steps)
+        SpinnerValueFactory<Integer> startMinuteSpinner = new SpinnerValueFactory.IntegerSpinnerValueFactory(
+            0, 59, 0, 15 // (min, max, default, step)
+        ); 
+        startMinute.setValueFactory(startMinuteSpinner);
+
+        SpinnerValueFactory<Integer> endMinuteSpinner = new SpinnerValueFactory.IntegerSpinnerValueFactory(
+            0, 59, 0, 15
+        );
+        endMinute.setValueFactory(endMinuteSpinner);
+
         // Get User Details
         user = customerDAO.getCustomer(parseInt(userID));
     }
@@ -72,14 +105,23 @@ public class User_reserveBikeController {
             LocalDate startDate = startDateSelection.getValue();
             LocalDate endDate = endDateSelection.getValue();
 
+            // Get time values 
+            int sHour = startHour.getValue();
+            int sMin = startMinute.getValue();
+            int eHour = endHour.getValue();
+            int eMin = endMinute.getValue();
+
+            LocalDateTime startDateTime = startDate.atTime(sHour, sMin);
+            LocalDateTime endDateTime = endDate.atTime(eHour, eMin);
+
             // Warn user for any reservation errors. If fails, stop
             if (!validateReservation(startDate, endDate)) {
                 return; 
             }
 
             // Convert localDate to Timestamp
-            Timestamp startTimestamp = Timestamp.valueOf(startDate.atStartOfDay());
-            Timestamp endTimestamp = Timestamp.valueOf(endDate.atStartOfDay());
+            Timestamp startTimestamp = Timestamp.valueOf(startDateTime);
+            Timestamp endTimestamp = Timestamp.valueOf(endDateTime);
 
             // Check if reservation already exists
             if (reservationDAO.hasOverlappingReservation(selectedBike.getBikeID(), startTimestamp, endTimestamp)) {
@@ -111,16 +153,40 @@ public class User_reserveBikeController {
 
     private BigDecimal calculatePrice(bikeReservation reservation) {
 
-        // Get number of days in rental
-        long days = java.time.temporal.ChronoUnit.DAYS.between(
-            reservation.getStartDate().toInstant(), 
-            reservation.getEndDate().toInstant()
-        );
+        // Get the rates from bike model
+        BigDecimal hourlyRate = selectedBike.getHourlyRate();
+        BigDecimal dailyRate = selectedBike.getDailyRate();
 
-        if (days == 0) {days = 1; } // Minimum 1 day
-        BigDecimal amount = selectedBike.getDailyRate().multiply(new BigDecimal(days));
+        // Get start and end times
+        LocalDateTime start = reservation.getStartDate().toLocalDateTime();
+        LocalDateTime end = reservation.getEndDate().toLocalDateTime();
+        
+        // Calculate total hours (rounded up)
+        long totalMinutes = java.time.temporal.ChronoUnit.MINUTES.between(start, end);
+        if (totalMinutes <= 0) { totalMinutes = 60; } // Min 1 hour
+        
+        // Add any minutes totaling to an hour
+        long totalHours = (totalMinutes / 60);
+        if (totalMinutes % 60 > 0) {
+            totalHours++; 
+        }
 
-        return amount;
+        // Add any hours totaling to days
+        long totalDays = (totalHours / 24);
+        if (totalHours % 24 > 0) {
+            totalDays++; 
+        }
+        
+        // Calculate hourly and daily price
+        BigDecimal priceByHour = hourlyRate.multiply(new BigDecimal(totalHours));
+        BigDecimal priceByDay = dailyRate.multiply(new BigDecimal(totalDays));
+
+        // Compare prices and return the cheaper one        
+        if (priceByDay.compareTo(priceByHour) < 0) {
+            return priceByDay; 
+        } else {
+            return priceByHour;
+        }
     }
 
     private boolean validateReservation(LocalDate startDate, LocalDate endDate) {
