15851c06277b2463b3a921f15b068832264292a8
ig21xn-dlsu <ian_gabriel_ilagan@dlsu.edu.ph>
2025-11-19 20:31:52 +0800

    Added "CREATE DATABASE IF NOT EXISTS ccinfom;"

--- src/main/resources/ccinfom.sql
+++ src/main/resources/ccinfom.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `admin`
 --
 
+CREATE DATABASE IF NOT EXISTS ccinfom;
+
 DROP TABLE IF EXISTS `admin`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
